34544691db4ce5ba335d809cc341b186a54c552c
ElfMmmk <48256365+ElfMmmk@users.noreply.github.com>
2023-10-24 22:49:59 +0300

    api: add linter

--- backend/api/models.py
+++ backend/api/models.py
@@ -22,7 +22,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     def __str__(self):
-        return self.email
+        return str(self.email)
 
     class Meta:
         verbose_name = 'Пользователь'
